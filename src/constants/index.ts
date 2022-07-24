72c3b6a4e478210a343f6f2bcc44cc026e76cbab
JSadept <jsadept4@gmail.com>
2022-07-24 14:59:09 +0300

    Add basic constants

---
+++ src/constants/index.ts
@@ -0,0 +1,17 @@
+
+
+export const SERVER_BASE_URL = "http://localhost:5500";
+export const WEBSOCKET_BASE_URL = "http://localhost:3001";
+
+export const API_BASE_URL = SERVER_BASE_URL + "/api";
+export const WEBSOCKET_URL = WEBSOCKET_BASE_URL + "/websocket";
+
+
+// auth
+export const AUTH_LOGIN = "/auth/login";
+export const AUTH_REG = "/auth/register";
+
+
+// room
+export const ROOM_CREATE = "/room/createRoom";
+export const ROOM_JOIN = "/room/join";
