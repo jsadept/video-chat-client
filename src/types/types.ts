23150e30b5675c178b0d7deb35dbdedffb63437c
JSadept <jsadept4@gmail.com>
2022-07-24 12:10:19 +0300

    Add types

---
+++ src/types/types.ts
@@ -0,0 +1,41 @@
+
+
+
+export interface IMediaData {
+    videoSources: [];
+    audioSources: [];
+    userStream: MediaStream;
+    streams: IStreamData[];
+}
+
+export interface IStreamData {
+    isVideo: boolean;
+    isAudio: boolean;
+    user: IUser;
+}
+
+export interface IUser {
+    userId?: string;
+    email: string;
+    nickname: string;
+    roles: string[];
+    isAuth: boolean;
+    socketData: [];
+    peerData: [];
+    mediaData: IMediaData;
+}
+
+export interface UserResponse{
+    userId?: string;
+    email: string;
+    nickname: string;
+    roles: string[];
+}
+
+export enum LoadingStatus {
+    LOADED = "LOADED",
+    LOADING = "LOADING",
+    ERROR = "ERROR",
+    NEVER = "NEVER",
+    SUCCESS = "SUCCESS"
+}
