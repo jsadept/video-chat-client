cf4c6e26513277c8d2aa87f8bc0eed0be5ac6d1e
JSadept <jsadept4@gmail.com>
2022-08-07 13:10:52 +0300

    Add components/AppRouter

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import {Navigate, Route, Routes } from 'react-router-dom';
+import {useAppSelector} from "../../hooks/redux";
+import {IRoute, privateRoutes, publicRoutes, RouteNames } from '../../routes/routes';
+import {selectAuthIsLogin} from "../../store/auth/auth-selector";
 
 const AppRouter = () => {
+    const isAuth = useAppSelector(selectAuthIsLogin);
+
     return (
-        <div>
-            
-        </div>
+        isAuth ?
+            <Routes>
+                {privateRoutes.map((route: IRoute) =>
+                    <Route path={route.path}
+                           key={route.path}
+                           element={<route.component />}
+                    />
+                )}
+                <Route
+                    path="*"
+                    element={<Navigate to={RouteNames.HOME} replace />}
+                />
+            </Routes>
+            :
+            <Routes>
+                {publicRoutes.map((route: IRoute) =>
+                    <Route path={route.path}
+                           key={route.path}
+                           element={<route.component />}
+                    />
+                )}
+                <Route
+                    path="*"
+                    element={<Navigate to={RouteNames.LOGIN} replace />}
+                />
+            </Routes>
     );
 };
 
