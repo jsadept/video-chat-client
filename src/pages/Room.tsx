f81c6e777b6546c9f8b6927718fe34c39b179877
JSadept <jsadept4@gmail.com>
2022-08-29 11:44:29 +0300

    Update Room page

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,61 +1,39 @@
-import {Box} from '@mui/material';
-import React from 'react';
-// import {useAuth} from "../hooks/useAuth";
-
-import {useLocation} from 'react-router-dom';
-import MeetNavigation from "../components/Meet/MeetNavigation";
-import MeetAdditional from "../components/Meet/MeetAdditional";
-import WebcamList from '../components/Meet/WebcamList';
-// import {useRemoteStreams} from "../hooks/useRemoteStreams";
-// import WebCamList from "../components/Meet/WebCamList";
-// import ChatNavigation from "../components/Chat/ChatNavigation";
-// import MeetAdditional from "../components/Meet/MeetAdditional";
-// import MeetNavigation from '../components/Meet/MeetNavigation';
-
-interface locState {
-    roomId: string;
-    myPeerID: string;
-}
+import {Box, Grid} from '@mui/material';
+import React, {useEffect, useState} from 'react';
+import BasicLayout from "../components/Layout/BasicLayout";
+import Meet from '../components/Meet/Meet';
+import {useAppDispatch, useAppSelector} from "../hooks/redux";
+import {getStreamSources, updateStream } from '../store/media/media-thunks';
+import {peerConnect, peerDisconnect} from '../store/peer/peer-thunks';
+import {joinRoom, leaveRoom} from "../store/socket/socket-thunks";
+import {selectSocketCurrentRoomState} from "../store/socket/socket-selector";
+import {getCurrentDate} from "../utils/getCurrentDate";
 
 const Room = () => {
+    const dispatch = useAppDispatch();
+    const currentRoomId = useAppSelector(selectSocketCurrentRoomState)
 
 
-    const location = useLocation().state as locState;
-    console.log(location)
-    console.log(useLocation())
-    //
-    // let roomId = location.roomId;
-    // let myPeerID = location.myPeerID;
-    //
-    // const currentSocketId = socket.getSocket().id;
-    // console.log(currentSocketId, roomId, myPeerID)
-    //const result = auth.joinRoom(currentSocketId, roomId, myPeerID);
-    //
-    // const videoRef = useRef<HTMLVideoElement>(null);
+    useEffect(() => {
+        // dispatch(peerConnect());
+        dispatch(joinRoom(currentRoomId));
+        // dispatch(updateStream());
+        // dispatch(getStreamSources());
 
-    // navigator.mediaDevices.getUserMedia({
-    //     audio: true,
-    //     video: true,
-    // }).then((stream) => {
-    //     if(videoRef.current){
-    //         // videoRef.current.srcObject = stream;
-    //         return stream;
-    //     }
-    // });
+        return () => {
 
-    // const currentSocketId = socket.getSocket().id;
-    // const {remoteStreams, addRemoteStream, removeRemoteStream} = useRemoteStreams();
+            dispatch(peerDisconnect());
+            dispatch(leaveRoom());
+
+        }
+    }, [])
 
 
     return(
-        <Box sx={{height: '100%', display: 'flex', p: 1}}>
-            <Box sx={{flexGrow: 1, display: 'flex', flexDirection: 'column'}}>
-                <WebcamList />
-                <MeetNavigation />
-            </Box>
-            <Box sx={{flexBasis: '20%', p: 1}}>
-                <MeetAdditional />
-            </Box>
+        <Box sx={{ width: '100%', padding: 0    }}>
+            <BasicLayout>
+                <Meet />
+            </BasicLayout>
         </Box>
     );
 };
