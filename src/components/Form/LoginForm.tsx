3dba9abd1416ac74dba9cfd9d62e74a81b573d00
JSadept <jsadept4@gmail.com>
2022-09-03 12:57:09 +0300

    Update LoginForm, RegisterForm: add validation, refactor

--- src/components/Form/LoginForm.tsx
+++ src/components/Form/LoginForm.tsx
@@ -1,18 +1,20 @@
-import {Visibility, VisibilityOff } from '@mui/icons-material';
-import {Box,
-    Button, FormControl, FormHelperText, IconButton, Input, InputAdornment, InputLabel, OutlinedInput, TextField } from '@mui/material';
-import React, {useEffect} from 'react';
+import {Visibility, VisibilityOff} from '@mui/icons-material';
+import {Alert, Box, Button, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput} from '@mui/material';
+import React, {useEffect, useState} from 'react';
 import {useNavigate} from "react-router-dom";
-import { RouteNames } from '../../routes/routes';
+import {RouteNames} from '../../routes/routes';
 import {useAppDispatch, useAppSelector} from "../../hooks/redux";
 import {login} from "../../store/auth/auth-thunks";
-import {selectAuthIsLogin} from "../../store/auth/auth-selector";
+import {selectAuthErrorMessage, selectAuthIsLogin} from "../../store/auth/auth-selector";
+import {isValidPassword} from "../../utils/isValidPassword";
+import {isValidEmail} from "../../utils/isValidEmail";
 
 
 const LoginForm = () => {
 
     const dispatch = useAppDispatch();
     const isAuth = useAppSelector(selectAuthIsLogin);
+    const authError = useAppSelector(selectAuthErrorMessage);
     const navigate = useNavigate();
 
     const [values, setValues] = React.useState({
@@ -20,6 +22,11 @@ const LoginForm = () => {
         password: '',
         showPassword: false,
     });
+
+
+    const [loginError, setLoginError] = useState<boolean>(false);
+    const [passwordError, setPasswordError] = useState<boolean>(false);
+    const [emailError, setEmailError] = useState<boolean>(false);
 
     const handleChange =
         (prop: any) => (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -38,58 +45,92 @@ const LoginForm = () => {
     };
 
     const submitForm = async () => {
-        dispatch(login({email: values.email, password: values.password}))
+        setPasswordError(false);
+        setEmailError(false);
+
+
+        if(!isValidPassword(values.password) && !isValidEmail(values.email)) {
+            if(!isValidPassword(values.password)) setPasswordError(true);
+            if(!isValidEmail(values.email)) setEmailError(true);
+        }
+        else{
+            dispatch(login({email: values.email, password: values.password}));
+        }
+
     }
 
+
     useEffect(() => {
+        setLoginError(false);
         if(isAuth){
             navigate(RouteNames.HOME);
-        }else{
+        }
+        else if(!isAuth && authError){
+            setLoginError(true);
             setValues({
                 email: '',
                 password: '',
                 showPassword: false,
             })
         }
-    }, [isAuth])
+        else{
+            setValues({
+                email: '',
+                password: '',
+                showPassword: false,
+            })
+        }
+    }, [isAuth, authError])
 
     return (
-        <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', padding: '0' }}>
-            <FormControl sx={{ m: 1, width: '25ch', height: '56px' }} variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-email">Email</InputLabel>
-                <OutlinedInput
-                    id="outlined-adornment-email"
-                    type={'email'}
-                    value={values.email}
-                    onChange={handleChange('email')}
-                    label="Password"
-                />
-            </FormControl>
-            <FormControl sx={{ m: 1, width: '25ch', height: '56px' }} variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                <OutlinedInput
-                    id="outlined-adornment-password"
-                    type={values.showPassword ? 'text' : 'password'}
-                    value={values.password}
-                    onChange={handleChange('password')}
-                    endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={handleClickShowPassword}
-                                onMouseDown={handleMouseDownPassword}
-                                edge="end"
-                            >
-                                {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                            </IconButton>
-                        </InputAdornment>
-                    }
-                    label="Password"
-                />
-            </FormControl>
-            <FormControl sx={{ m: 1, width: '25ch', height: '56px' }} variant="outlined">
-                <Button onClick={() => submitForm()} sx={{height: '100%'}} variant="outlined">Login</Button>
-            </FormControl>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            {loginError && (
+                <Alert variant={"outlined"} severity="error">Login error. Try again</Alert>
+            )}
+            <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', padding: '0', flexDirection: 'column', alignItems: 'center'  }}>
+                <FormControl sx={{ m: 1, width: '25ch', height: '56px', marginBottom: emailError ? '65px' : '0px' }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-email">Email</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-email"
+                        type={'email'}
+                        value={values.email}
+                        onChange={handleChange('email')}
+                        label="Email"
+                    />
+                    {emailError && (
+                        <Alert severity="error">Email is invalid</Alert>
+                    )}
+
+                </FormControl>
+                <FormControl sx={{ m: 1, width: '25ch', height: '56px', marginBottom: passwordError ? '65px' : '0px' }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        type={values.showPassword ? 'text' : 'password'}
+                        value={values.password}
+                        onChange={handleChange('password')}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="Password"
+                    />
+                    {passwordError && (
+                        <Alert severity="error">Password must contain at least 6 characters</Alert>
+                    )}
+                </FormControl>
+                <FormControl sx={{ m: 1, width: '25ch', height: '56px' }} variant="outlined">
+                    <Button onClick={() => submitForm()} sx={{height: '100%'}} variant="outlined">Login</Button>
+                </FormControl>
+            </Box>
         </Box>
     );
 };
--- src/components/Form/RegisterForm.tsx
+++ src/components/Form/RegisterForm.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
-import {Box, Button, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput} from "@mui/material";
+import React, {useEffect, useState} from 'react';
+import {Alert, Box, Button, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput} from "@mui/material";
 import {Visibility, VisibilityOff} from "@mui/icons-material";
 import {useNavigate} from "react-router-dom";
+import {isValidPassword} from "../../utils/isValidPassword";
+import {isValidEmail} from "../../utils/isValidEmail";
+import {registration} from "../../store/auth/auth-thunks";
+import {useAppDispatch, useAppSelector} from "../../hooks/redux";
+import {RouteNames} from "../../routes/routes";
+import {selectAuthErrorMessage, selectAuthIsLogin, selectAuthIsRegistered} from "../../store/auth/auth-selector";
 
 const RegisterForm = () => {
 
+    const dispatch = useAppDispatch();
     const navigate = useNavigate();
+    const isRegistered = useAppSelector(selectAuthIsRegistered);
+    const authError = useAppSelector(selectAuthErrorMessage);
 
     const [values, setValues] = React.useState({
         email: '',
+        nickname: '',
         password: '',
         showPassword: false,
     });
+
+    const [registrationError, setRegistrationError] = useState<boolean>(false);
+    const [passwordError, setPasswordError] = useState<boolean>(false);
+    const [emailError, setEmailError] = useState<boolean>(false);
 
     const handleChange =
         (prop: any) => (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -30,47 +44,104 @@ const RegisterForm = () => {
     };
 
     const submitForm = async () => {
+        setPasswordError(false);
+        setEmailError(true);
+
+
+        if(!isValidPassword(values.password) && !isValidEmail(values.email)) {
+            if(!isValidPassword(values.password)) setPasswordError(true);
+            if(!isValidEmail(values.email)) setEmailError(true);
+        }
+        else{
+            dispatch(registration({nickName: values.nickname, email: values.email, password: values.password}));
+        }
 
     }
 
+
+    useEffect(() => {
+        if(isRegistered){
+            navigate(RouteNames.LOGIN);
+        }
+        else if(!isRegistered && authError){
+            setRegistrationError(true);
+            setValues({
+                nickname: '',
+                email: '',
+                password: '',
+                showPassword: false,
+            })
+        }
+        else{
+            setValues({
+                nickname: '',
+                email: '',
+                password: '',
+                showPassword: false,
+            })
+        }
+    }, [isRegistered, authError])
+
     return (
-        <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', padding: '0' }}>
-            <FormControl sx={{ m: 1, width: '25ch', height: '56px' }} variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-email">Email</InputLabel>
-                <OutlinedInput
-                    id="outlined-adornment-email"
-                    type={'email'}
-                    value={values.email}
-                    onChange={handleChange('email')}
-                    label="Password"
-                />
-            </FormControl>
-            <FormControl sx={{ m: 1, width: '25ch', height: '56px' }} variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                <OutlinedInput
-                    id="outlined-adornment-password"
-                    type={values.showPassword ? 'text' : 'password'}
-                    value={values.password}
-                    onChange={handleChange('password')}
-                    endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={handleClickShowPassword}
-                                onMouseDown={handleMouseDownPassword}
-                                edge="end"
-                            >
-                                {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                            </IconButton>
-                        </InputAdornment>
-                    }
-                    label="Password"
-                />
-            </FormControl>
-            <FormControl sx={{ m: 1, width: '25ch', height: '56px' }} variant="outlined">
-                <Button onClick={() => submitForm()} sx={{height: '100%'}} variant="outlined">Create user</Button>
-            </FormControl>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            {registrationError && (
+                <Alert variant={"outlined"} severity="error">Registration error. Try again</Alert>
+            )}
+            <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', padding: '0', flexDirection: 'column', alignItems: 'center' }}>
+                <FormControl sx={{ m: 1, width: '25ch', height: '56px' }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-nickname">Nickname</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-nickname"
+                        type={'text'}
+                        value={values.nickname}
+                        onChange={handleChange('nickname')}
+                        label="Nickname"
+                    />
+                </FormControl>
+                <FormControl sx={{ m: 1, width: '25ch', height: '56px' }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-email">Email</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-email"
+                        type={'email'}
+                        value={values.email}
+                        onChange={handleChange('email')}
+                        label="Email"
+                    />
+                    {emailError && (
+                        <Alert severity="error">Email is invalid</Alert>
+                    )}
+                </FormControl>
+                <FormControl sx={{ m: 1, width: '25ch', height: '56px' }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        type={values.showPassword ? 'text' : 'password'}
+                        value={values.password}
+                        onChange={handleChange('password')}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="Password"
+                    />
+                    {passwordError && (
+                        <Alert severity="error">Password must contain at least 6 characters</Alert>
+                    )}
+                </FormControl>
+                <FormControl sx={{ m: 1, width: '25ch', height: '56px' }} variant="outlined">
+                    <Button onClick={() => submitForm()} sx={{height: '100%'}} variant="outlined">Create user</Button>
+                </FormControl>
+            </Box>
         </Box>
+
     );
 };
 
